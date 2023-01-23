82457e20d7968db6b9c697e19c5dc1524cd9f2b8
Marc Paul <mpaul0150@gmail.com>
2023-01-23 23:47:28 +0100

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,6 +1,6 @@
 
     const eye1 = document.querySelector('.eye-left');
-    const eye2 = document.querySelector('.eye-right')
+    const eye2 = document.querySelector('.eye-right');
     window.addEventListener('mousemove', (evt) => {
       var x = -(window.innerWidth / 2 - evt.pageX) / 2;
       var y = -(window.innerHeight / 2 - evt.pageY) / 2;
